12ec2fb22b0e53d6146ab6c0365f51f6cc7d0305
parth gadhavi <162983927+parthbhagadhavi@users.noreply.github.com>
2024-05-12 16:13:45 +0530

    Add files via upload

---
+++ squerepattern4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int rows = 5, cols = 5;
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            if(i == 0 || i == rows - 1 || j == 0 || j == cols - 1 || i == j || j == cols - i - 1)
+                printf("* ");
+            else
+                printf("  "); 
+        }
+        printf("\n"); 
+    }
+}
+
+
+
+    
